105d9508d888d6e4d0ad28b037857bbb24b91515
Murat Yirci <myirci@gmail.com>
2022-05-31 00:50:42 -0500

    Some additional clean-up.

--- ChessLib/Source/ChessLib/Board/BasicBoard.hpp
+++ ChessLib/Source/ChessLib/Board/BasicBoard.hpp
@@ -64,67 +64,6 @@ namespace chesslib
 
 		BasicBoard() noexcept : PieceCentricBoardBase() { _board.fill(Empty); }
 
-		/*
-
-		template<Color Clr>
-		void GenerateMovesImplementation(MoveList& moves) const;
-
-		template<Color Attacker>
-		void ComputeChecksAndPins(Square king_pos) const;
-
-		template<Color Attacker, bool IsStraightMovingPiece>
-		void ComputeChecksAndPins(Square king_pos, const std::array<Direction, 4>& attack_directions) const;
-
-		template<Color Clr>
-		void GenerateKingMoves(Square king_pos, MoveList& moves) const;
-
-		template<Color Attacker>
-		inline bool IsDirectionInvalidForKing(Direction dir) const;
-
-		template<Color Attacker>
-		bool IsUnderAttack(Square sq) const;
-
-		template<Color Attacker, bool IsStraightMovingPiece>
-		bool IsUnderAttack(Square sq, const std::array<Direction, 4>& attack_directions) const;
-
-		template<Color Attacker>
-		bool IsUnderAttackByAKnight(Square sq) const;
-
-		template<Color Clr>
-		inline bool IsKingSideCastlingPossible() const;
-
-		template<Color Clr>
-		inline bool IsQueenSideCastlingPossible() const;
-
-		// Generates check blocking and checker capturing moves
-		template<Color Clr>
-		void GenerateCheckEliminatingMoves(Square king_pos, MoveList& moves) const;
-
-		template<Color Clr>
-		void GenerateCheckEliminatingEnPassantCaptureMoves(Square attacker_loc, MoveList& moves) const;
-
-		// Generates non-king moves to the given square
-		template<Color Clr>
-		void ToSquareMoves(Square sq, MoveList& moves) const;
-
-		template<Color Clr>
-		void GenerateKnightMoves(MoveList& moves) const;
-
-		// Generates non-king piece moves that move straight (horizontal/vertical)
-		template<Color Clr>
-		void GenerateStraightMoves(MoveList& moves) const;
-
-		// Generates non-king piece moves that move diagonally
-		template<Color Clr>
-		void GenerateDiagonalMoves(MoveList& moves) const;
-
-		template<Color Clr>
-		void GeneratePawnMoves(MoveList& moves) const;
-
-		template<Color Clr>
-		void GenerateEnPassantCaptureMoves(Square king_pos, MoveList& moves) const;
-		*/
-
 		friend BoardFactory;
 	};
 }
